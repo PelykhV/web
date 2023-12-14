e24f72bf24a7f69c66857d8d7363e3e05540b838
PelykhV <119511581+PelykhV@users.noreply.github.com>
2023-12-14 23:46:50 +0200

    Add files via upload

---
+++ src/Pages/Catalog/Catalog.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+export const CardWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 20px;
+  flex-wrap: wrap;
+`;
+
+export const PriceSection = styled.div`
+  display: flex; 
+  align-items: center;
+  width: 75%;
+  margin-left: 90px;
+`;
+
+export const VerticalLine = styled.hr`
+  width: 100%;
+  border-bottom: none;
+  border-top: 2px solid #e1e1e1;
+`;
